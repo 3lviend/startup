c75b90df92e852b0104d4f64e11a27984c41a013
3lviend <henryss_10@yahoo.com>
2013-03-16 00:38:48 +0700

    mailer setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,13 +36,13 @@ StartupJob::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-#  config.action_mailer.delivery_method = :smtp
-#  config.action_mailer.smtp_settings = {
-#    :address              => "smtp.gmail.com",
-#    :port                 => 587,
-#    :domain               => 'elvin.alvian.siagian@gmail.com',
-#    :user_name            => '<username>',
-#    :password             => '<password>',
-#    :authentication       => 'plain',
-#    :enable_starttls_auto => true  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'elvin.alvian.siagian',
+    :password             => '5t4r3e2w1q',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ StartupJob::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'elvin.alvian.siagian',
+    :password             => '5t4r3e2w1q',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
